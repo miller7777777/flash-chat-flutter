5d37c7963ac3020b1c2f8d3cf9ae619dfc36e14f
Miller777 <miller777@mail.ru>
2021-02-02 01:06:48 +0300

    Рефакторинг. Изменил имена маршрутов.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -15,7 +18,14 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      // home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (BuildContext context) => WelcomeScreen(),
+        '/registration': (BuildContext context) => RegistrationScreen(),
+        '/login': (BuildContext context) => LoginScreen(),
+        '/chat': (BuildContext context) => ChatScreen(),
+      },
     );
   }
 }
