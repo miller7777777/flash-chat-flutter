e621ee64d4ea8087d1ba7fef413acbef7f622679
Miller777 <miller777@mail.ru>
2021-02-10 01:32:20 +0300

    Начал делать Animation.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +10,17 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: vsync);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
